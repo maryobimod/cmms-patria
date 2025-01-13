d00d7c77154e15524310c2b3b720838b42e79dde
mstkm <mustakimmugi@gmail.com>
2025-01-13 07:59:53 +0700

    show name pid

--- src/components/RealtimeLineChart1.tsx
+++ src/components/RealtimeLineChart1.tsx
@@ -142,37 +142,37 @@ export default function RealtimeLineChart1() {
           labels: labels,
           datasets: [
             {
-              label: "PID 1",
+              label: "Voltage Measured",
               data: pid1Value,
               borderColor: "#1c538a",
               fill: false,
             } as any,
             {
-              label: "PID 2",
+              label: "Current Measured",
               data: pid2Value,
               borderColor: "#a2c7ff",
               fill: false,
             } as any,
             {
-              label: "PID 3",
+              label: "Total kWh Machine",
               data: pid3Value,
               borderColor: "#2f7ef4",
               fill: false,
             } as any,
             {
-              label: "PID 4",
+              label: "Instantaneous Power Consumption",
               data: pid4Value,
               borderColor: "#30f2f2",
               fill: false,
             } as any,
             {
-              label: "PID 5",
+              label: "Reactive Power Absorbed by Machine",
               data: pid5Value,
               borderColor: "#74e0a5",
               fill: false,
             } as any,
             {
-              label: "PID 6",
+              label: "Reactive Power Supplied by Machine",
               data: pid6Value,
               borderColor: "#9b46bf",
               fill: false,
@@ -197,30 +197,30 @@ export default function RealtimeLineChart1() {
       <div>
         <Line data={chartData} options={options} />
       </div>
-      <div className="flex justify-center items-center gap-2 text-[12px] mt-1">
+      <div className="flex justify-center items-center gap-2 text-[12px] mt-2 px-8">
         <div className="flex justify-center items-center gap-1">
           <div className="w-1 p-1 bg-[#1c538a] rounded-full"></div>
-          <div>PID 1</div>
+          <div>Voltage Measured</div>
         </div>
         <div className="flex justify-center items-center gap-1">
           <div className="w-1 p-1 bg-[#a2c7ff] rounded-full"></div>
-          <div>PID 2</div>
+          <div>Current Measured</div>
         </div>
         <div className="flex justify-center items-center gap-1">
           <div className="w-1 p-1 bg-[#2f7ef4] rounded-full"></div>
-          <div>PID 3</div>
+          <div>Total kWh Machine</div>
         </div>
         <div className="flex justify-center items-center gap-1">
           <div className="w-1 p-1 bg-[#30f2f2] rounded-full"></div>
-          <div>PID 4</div>
+          <div>Instantaneous Power Consumption</div>
         </div>
         <div className="flex justify-center items-center gap-1">
           <div className="w-1 p-1 bg-[#74e0a5] rounded-full"></div>
-          <div>PID 5</div>
+          <div>Reactive Power Absorbed by Machine</div>
         </div>
         <div className="flex justify-center items-center gap-1">
           <div className="w-1 p-1 bg-[#9b46bf] rounded-full"></div>
-          <div>PID 6</div>
+          <div>Reactive Power Supplied by Machine</div>
         </div>
       </div>
     </div>
